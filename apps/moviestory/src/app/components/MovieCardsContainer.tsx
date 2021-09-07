fec0a33a41e0e03a9dc8acaf8166c34b1e545b26
Bekzodjon Norkuziev <bekzodnx@gmail.com>
2021-09-07 22:59:37 +0200

    feat: separated MovieCardsContainer component

---
+++ apps/moviestory/src/app/components/MovieCardsContainer.tsx
@@ -0,0 +1,160 @@
+import styled from 'styled-components';
+
+import moviePoster from '../../assets/movie-posters/dark-knight.jpg';
+import matrixPoster from '../../assets/movie-posters/vanilla.jpg';
+
+const MovieCardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 1em 20em;
+  color: white;
+  border: 1px solid red;
+`;
+
+const MovieCard = styled.div`
+  & > img {
+    display: inline-block;
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+      rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+      rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+  }
+`;
+
+const MovieCard2 = styled.div`
+  width: 158px;
+  height: 234px;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+    rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+    rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+  background: url(${matrixPoster});
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+const MovieText = styled.div`
+  margin-top: 0.5em;
+  text-align: left;
+  font-size: 1em;
+  font-family: 'Noto Sans', sans-serif;
+  & > span {
+    display: block;
+  }
+  & > span:nth-child(2) {
+    color: #ffffff6e;
+    font-size: 0.9em;
+  }
+`;
+
+const MovieCardWrap = styled.div`
+  flex: 0 0 18%;
+  margin: 0.8em 1.3em;
+  border: 1px solid yellow;
+  & > * {
+    display: block;
+  }
+`;
+export const MovieCardsContainer = () => {
+  return (
+    <MovieCardContainer>
+      <MovieCardWrap>
+        <MovieCard>
+          <img
+            style={{ maxHeight: '100%', maxWidth: '100%' }}
+            src={moviePoster}
+            alt="Nx - Smart, Extensible Build Framework"
+          />
+        </MovieCard>
+        <MovieText>
+          <span>Batman Begins</span>
+          <span>Action, Drama</span>
+        </MovieText>
+      </MovieCardWrap>
+      <MovieCardWrap>
+        <MovieCard2></MovieCard2>
+        <MovieText>
+          <span>Matrix</span>
+          <span>Action, Sci-fi</span>
+        </MovieText>
+      </MovieCardWrap>
+      <MovieCardWrap>
+        <MovieCard>
+          <img
+            style={{ maxHeight: '100%', maxWidth: '100%' }}
+            src={moviePoster}
+            alt="Nx - Smart, Extensible Build Framework"
+          />
+        </MovieCard>
+        <MovieText>
+          <span>Batman Begins</span>
+          <span>Action, Drama</span>
+        </MovieText>
+      </MovieCardWrap>
+      <MovieCardWrap>
+        <MovieCard>
+          <img
+            style={{ maxHeight: '100%', maxWidth: '100%' }}
+            src={moviePoster}
+            alt="Nx - Smart, Extensible Build Framework"
+          />
+        </MovieCard>
+        <MovieText>
+          <span>Batman Begins</span>
+          <span>Action, Drama</span>
+        </MovieText>
+      </MovieCardWrap>
+      <MovieCardWrap>
+        <MovieCard>
+          <img
+            style={{ maxHeight: '100%', maxWidth: '100%' }}
+            src={moviePoster}
+            alt="Nx - Smart, Extensible Build Framework"
+          />
+        </MovieCard>
+        <MovieText>
+          <span>Batman Begins</span>
+          <span>Action, Drama</span>
+        </MovieText>
+      </MovieCardWrap>
+      <MovieCardWrap>
+        <MovieCard>
+          <img
+            style={{ maxHeight: '100%', maxWidth: '100%' }}
+            src={moviePoster}
+            alt="Nx - Smart, Extensible Build Framework"
+          />
+        </MovieCard>
+        <MovieText>
+          <span>Batman Begins</span>
+          <span>Action, Drama</span>
+        </MovieText>
+      </MovieCardWrap>
+      <MovieCardWrap>
+        <MovieCard>
+          <img
+            style={{ maxHeight: '100%', maxWidth: '100%' }}
+            src={moviePoster}
+            alt="Nx - Smart, Extensible Build Framework"
+          />
+        </MovieCard>
+        <MovieText>
+          <span>Batman Begins</span>
+          <span>Action, Drama</span>
+        </MovieText>
+      </MovieCardWrap>
+      <MovieCardWrap>
+        <MovieCard>
+          <img
+            style={{ maxHeight: '100%', maxWidth: '100%' }}
+            src={moviePoster}
+            alt="Nx - Smart, Extensible Build Framework"
+          />
+        </MovieCard>
+        <MovieText>
+          <span>Batman Begins</span>
+          <span>Action, Drama</span>
+        </MovieText>
+      </MovieCardWrap>
+    </MovieCardContainer>
+  );
+};
